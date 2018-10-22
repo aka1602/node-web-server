232cc25aeb289e7c77587cb40cf0dd5624233100
akash <akash.greenwork1@gmail.com>
2018-10-22 23:16:50 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs = require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
 var app=express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -54,4 +55,4 @@ app.get('/bad',(req,res)=>{
     errorMessage: 'unable to connect to server'
   });
 });
-app.listen(3000);
+app.listen(port);
