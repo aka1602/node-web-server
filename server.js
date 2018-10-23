d97d2092d9a86cd621b32a34773e424254c1233f
akash <akash.greenwork1@gmail.com>
2018-10-23 09:16:49 +0530

    added new project.hbs

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get('/about',(req,res)=>{
 
   });
 });
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects'
+  });
+});
 app.get('/home',(req,res)=>{
   res.render('home.hbs',{
     pageTitle: 'home page',
